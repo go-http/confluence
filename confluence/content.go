d1366de826354a93ac64206b5be0c8c8782cdc22
Jianbo Feng <fengjianbo@nibirutech.com>
2018-11-15 19:11:27 +0800

    增加搜索指定空间指定标题Content的功能

--- confluence/content.go
+++ confluence/content.go
@@ -3,6 +3,7 @@ package confluence
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"net/url"
 )
 
@@ -64,4 +65,42 @@ func (cli *Client) ContentByIdWithOpt(id string, opt url.Values) (Content, error
 	}
 
 	return info.Content, nil
+}
+
+func (cli *Client) ContentBySpaceAndTitle(space, title string) (Content, error) {
+	q := url.Values{
+		"title":    {title},
+		"spaceKey": {space},
+		"expand":   {"version"},
+	}
+
+	resp, err := cli.GET("/content", q)
+	if err != nil {
+		return Content{}, fmt.Errorf("执行请求失败: %s", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return Content{}, fmt.Errorf("[%d]%s", resp.StatusCode, resp.Status)
+	}
+
+	var info struct {
+		PageResp
+		Results []Content
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&info)
+	if err != nil {
+		return Content{}, fmt.Errorf("解析响应失败: %s", err)
+	}
+
+	switch info.Size {
+	case 0:
+		return Content{}, nil
+	case 1:
+		return info.Results[0], nil
+	default:
+		return Content{}, fmt.Errorf("找到%d条记录", info.Size)
+	}
 }
