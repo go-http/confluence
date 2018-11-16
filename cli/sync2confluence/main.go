8f499f65c59acd73841e26721227480c0b071fbf
Jianbo Feng <fengjianbo@nibirutech.com>
2018-11-16 15:27:21 +0800

    添加导入指定目录到Confluence空间的cli范例

---
+++ cli/sync2confluence/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"confluencer"
+)
+
+func main() {
+	var addr, user, pass, space, dir string
+
+	flag.StringVar(&addr, "addr", "https://www.confluence.com", "Confluence访问地址")
+	flag.StringVar(&user, "u", "", "用户名")
+	flag.StringVar(&pass, "p", "", "密码")
+	flag.StringVar(&space, "s", "", "Confluence空间标识")
+	flag.StringVar(&dir, "d", "", "要导入的目录")
+
+	flag.Parse()
+
+	client := confluence.New(addr, user, pass)
+	err := client.SpaceContentImportFrom(space, dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
