e875d8049e1c9133ee8e784724168656dfb57cad
Jianbo Feng <fengjianbo@nibirutech.com>
2018-11-27 15:52:41 +0800

    调整request封装格式

--- request.go
+++ request.go
@@ -48,32 +48,33 @@ type PageResp struct {
 func (cli *Client) GET(path string, query url.Values) (*http.Response, error) {
 	return cli.Request("GET", path, query, nil)
 }
+
 func (cli *Client) POST(path string, data interface{}) (*http.Response, error) {
-	return cli.Request("POST", path, nil, data)
+	r, err := dataToJsonReader(data)
+	if err != nil {
+		return nil, fmt.Errorf("编码请求数据失败: %s", err)
+	}
+	return cli.Request("POST", path, nil, r)
 }
+
 func (cli *Client) PUT(path string, data interface{}) (*http.Response, error) {
-	return cli.Request("PUT", path, nil, data)
+	r, err := dataToJsonReader(data)
+	if err != nil {
+		return nil, fmt.Errorf("编码请求数据失败: %s", err)
+	}
+
+	return cli.Request("PUT", path, nil, r)
 }
 
 // 执行指定的HTTP请求，执行前会自动添加上认证信息和Content-Type信息
-func (cli *Client) Request(method, path string, query url.Values, data interface{}) (*http.Response, error) {
+func (cli *Client) Request(method, path string, query url.Values, body io.Reader) (*http.Response, error) {
 	// 检查添加Query参数
 	if query != nil {
 		path += "?" + query.Encode()
 	}
 
-	// 检查添加Body数据
-	var bodyReader io.Reader
-	if data != nil {
-		body, err := json.Marshal(data)
-		if err != nil {
-			return nil, fmt.Errorf("无法编码Data: %s", err)
-		}
-		bodyReader = bytes.NewReader(body)
-	}
-
 	// 构造请求
-	req, err := http.NewRequest(method, cli.Address+path, bodyReader)
+	req, err := http.NewRequest(method, cli.Address+path, body)
 	if err != nil {
 		return nil, fmt.Errorf("创建请求失败: %s", err)
 	}
@@ -82,4 +83,14 @@ func (cli *Client) Request(method, path string, query url.Values, data interface
 	req.SetBasicAuth(cli.Username, cli.Password)
 
 	return http.DefaultClient.Do(req)
+}
+
+// 数据转换为JSON流reader
+func dataToJsonReader(data interface{}) (io.Reader, error) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, fmt.Errorf("无法编码Data: %s", err)
+	}
+
+	return bytes.NewReader(jsonData), nil
 }
