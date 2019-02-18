80d44370e6766468de8a1bd86fb10c3f7c0af3ea
Jianbo Feng <fengjianbo@nibirutech.com>
2019-02-18 19:32:35 +0800

    调整blackfriday的引用地址

--- cli/sync2confluence/blackfriday.go
+++ cli/sync2confluence/blackfriday.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"gopkg.in/russross/blackfriday.v2"
 	"io"
 	"io/ioutil"
 	"net/url"
 	"path"
+
+	"github.com/russross/blackfriday/v2"
 )
 
 type BlackFridayRenderer struct {
